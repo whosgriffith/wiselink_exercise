3d8e636e1654d4dbcd5ed9c1e70fb34957b251b9
Nicolas <nicolasre011@gmail.com>
2022-08-30 02:42:09 -0300

    Added Account model for custom user

--- users/models.py
+++ users/models.py
@@ -7,7 +7,3 @@ from utils.models import BaseModel
 
 class Account(BaseModel, AbstractUser):
     organization_name = models.CharField(blank=True, null=True, max_length=255)
-    created_events = models.PositiveIntegerField(default=0)
-    finished_events = models.PositiveIntegerField(default=0)
-    cancelled_events = models.PositiveIntegerField(default=0)
-    attended_events = models.PositiveIntegerField(default=0)
